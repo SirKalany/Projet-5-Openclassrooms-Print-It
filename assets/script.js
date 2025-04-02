45abf915d967aa609b1005e4f55c445df4de12a3
Kalany <duncan.miard@outlook.fr>
2025-04-02 19:24:20 +0200

    Simplification du JS

--- assets/script.js
+++ assets/script.js
@@ -18,63 +18,46 @@ const slides = [
 ];
 
 let slideNmb = 0;
+let dotsContainer = document.querySelector(".dots");
 
 for (let i = 0; i < slides.length; i++) {
-  let dots = document.querySelector(".dots");
   let dot = document.createElement("div");
-  dot.className = "dot dot" + i;
+  dot.classList.add("dot", `dot${i}`);
   if (i === 0) {
-    dot.className = "dot " + "dot" + i + " dot_selected";
+    dot.classList.add("dot_selected");
   }
-  dots.appendChild(dot);
+  dotsContainer.appendChild(dot);
 }
 
 let bannerImage = document.querySelector(".banner-img");
 let bannerText = document.querySelector(".banner-text");
 
-function ChangeSlide() {
+function changeSlide() {
   bannerImage.src = `./assets/images/slideshow/${slides[slideNmb].image}`;
   bannerText.innerHTML = slides[slideNmb].tagLine;
 }
 
+function updateDots() {
+  document.querySelector(".dot_selected").classList.remove("dot_selected");
+  document.querySelector(`.dot${slideNmb}`).classList.add("dot_selected");
+}
+
 let leftArrow = document.getElementById("arrow_left");
 leftArrow.addEventListener("click", function () {
-  slideNmb--;
-
+  slideNmb -= 1;
   if (slideNmb < 0) {
-	slideNmb = slides.length-1;
+    slideNmb = slides.length - 1;
   }
-
-  let dotSelected = document.querySelector(".dot_selected");
-  dotSelected.className = "dot dot" + (slideNmb + 1);
-
-  if (slideNmb === slides.length - 1) {
-    dotSelected.className = "dot dot0";
-  }
-
-  dotSelected = document.querySelector(".dot" + slideNmb);
-  dotSelected.className = "dot " + "dot" + slideNmb + " dot_selected";
-
-  ChangeSlide();
+  updateDots();
+  changeSlide();
 });
 
 let rightArrow = document.getElementById("arrow_right");
 rightArrow.addEventListener("click", function () {
-  slideNmb++;
-
+  slideNmb += 1;
   if (slideNmb >= slides.length) {
-	slideNmb = 0;
+    slideNmb = 0;
   }
-
-  let dotSelected = document.querySelector(".dot_selected");
-  dotSelected.className = "dot dot" + (slideNmb - 1);
-
-  if (slideNmb === 0) {
-    dotSelected.className = "dot dot" + (slides.length - 1);
-  }
-
-  dotSelected = document.querySelector(".dot" + slideNmb);
-  dotSelected.className = "dot " + "dot" + slideNmb + " dot_selected";
-
-  ChangeSlide();
+  updateDots();
+  changeSlide();
 });
